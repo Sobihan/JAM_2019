ee1b68caa0ceedf2d87bd6dec5c267ddba307ee4
Sobihan <45632326+Sobihan@users.noreply.github.com>
2020-06-27 12:42:31 +0200

    Add files via upload

--- JAM_2019-master/Musicland/src/Bone.gd
+++ JAM_2019-master/Musicland/src/Bone.gd
@@ -1,18 +1,14 @@
 extends "ScrollMovement.gd"
 
-#export var player = []
+export var player = []
 
 func _physics_process(delta):
-#	if (player == [1, 1, 1, 1]):
-#		queue_free()
-#	print("-----")
-#	print("-----")
-#	print("Bone:")
-#	print(player)
+	if (player == [2, 1, 1, 1]):
+		queue_free()
 	move()
 
 
 func _on_Pickup_body_entered(body):
-#	pass
-	if body.name == "Player":
-		queue_free()
+	pass
+#	if body.name == "Player":
+#		queue_free()
--- JAM_2019-master/Musicland/src/Player.gd
+++ JAM_2019-master/Musicland/src/Player.gd
@@ -19,7 +19,8 @@ var state = RUN
 
 onready var animation = $PlayerAnimatedSprite
 
-onready var tab = preload("res://Barrel.tscn").duplicate()
+onready var tab = preload("res://Bone.tscn").duplicate()
+onready var barril = preload("res://Barrel.tscn").duplicate()
 
 func _ready():
 	get_viewport().audio_listener_enable_2d = true
@@ -33,14 +34,18 @@ var toClean = false
 	
 func _physics_process(delta):
 	var arr = tab.instance()
+	var b = barril.instance()
 	match state:
 		RUN:
 			animation.play("run")
 			if toClean:
 				toClean = false
+				b.player.clear()
 				arr.player.clear()
 			if (arr.player.size() > 4):
 				arr.player.pop_front()
+			if (b.player.size() > 4):
+				b.player.pop_front()
 		JUMP:
 			velocity = Vector2.ZERO
 			velocity.y -= jump_velocity
@@ -61,25 +66,30 @@ func playSound(path):
 
 func _input(event):
 	var arr = tab.instance()
+	var b = barril.instance()
 	if event.is_action_pressed("Jump") and state == RUN:
 		state = JUMP
 	elif event.is_action_pressed("ui_up") and key:
 		arr.player.push_back(1)
+		b.player.push_back(1)
 		key = false
 		timer.start()
 		$do.play()
 	elif event.is_action_pressed("ui_down") and key:
 		arr.player.push_back(2)
+		b.player.push_back(2)
 		key = false
 		timer.start()
 		$re.play()
 	elif event.is_action_pressed("ui_left") and key:
 		arr.player.push_back(3)
+		b.player.push_back(3)
 		key = false
 		timer.start()
 		$mi.play()
 	elif event.is_action_pressed("ui_right") and key:
 		arr.player.push_back(4)
+		b.player.push_back(4)
 		key = false
 		timer.start()
 		$fa.play()
