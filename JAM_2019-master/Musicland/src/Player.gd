a82798e82a788365843cd596eca6986305203c53
Sobihan <45632326+Sobihan@users.noreply.github.com>
2020-06-27 10:54:22 +0200

    Add files via upload

--- JAM_2019-master/Musicland/src/Player.gd
+++ JAM_2019-master/Musicland/src/Player.gd
@@ -17,6 +17,9 @@ var state = RUN
 onready var animation = $PlayerAnimatedSprite
 
 
+func _ready():
+	get_viewport().audio_listener_enable_2d = true
+	
 func _physics_process(delta):
 	match state:
 		RUN:
@@ -31,9 +34,25 @@ func _physics_process(delta):
 	velocity.y += gravity_scale
 	move_and_collide(velocity * delta)
 	
+
+func playSound(path):
+	var player = AudioStreamPlayer.new()
+	self.add_child(player)
+	player.stream = load(path)
+	player.play()
+
+
 func _input(event):
 	if event.is_action_pressed("Jump") and state == RUN:
 		state = JUMP
+	elif event.is_action_pressed("ui_up"):
+		$do.play()
+	elif event.is_action_pressed("ui_down"):
+		$re.play()
+	elif event.is_action_pressed("ui_left"):
+		$mi.play()
+	elif event.is_action_pressed("ui_right"):
+		$fa.play()
 
 func _on_Area2D_body_entered(body):
 	if body is StaticBody2D:
