acd59bda152b30123de732d8de14b4dea3867e6f
Sobihan <45632326+Sobihan@users.noreply.github.com>
2020-06-27 11:21:53 +0200

    Add files via upload

--- JAM_2019-master/Musicland/src/Player.gd
+++ JAM_2019-master/Musicland/src/Player.gd
@@ -5,6 +5,8 @@ var velocity = Vector2.ZERO
 export var jump_velocity = 500.0
 export var gravity_scale = 20.0
 
+onready var timer = get_node("Timer")
+
 #var can_jump = true
 enum {
 	JUMP,
@@ -16,9 +18,13 @@ var state = RUN
 
 onready var animation = $PlayerAnimatedSprite
 
-
 func _ready():
 	get_viewport().audio_listener_enable_2d = true
+	timer.set_wait_time(0.1)
+	timer.start()
+
+var key = true
+
 	
 func _physics_process(delta):
 	match state:
@@ -45,13 +51,21 @@ func playSound(path):
 func _input(event):
 	if event.is_action_pressed("Jump") and state == RUN:
 		state = JUMP
-	elif event.is_action_pressed("ui_up"):
+	elif event.is_action_pressed("ui_up") and key:
+		key = false
+		timer.start()
 		$do.play()
-	elif event.is_action_pressed("ui_down"):
+	elif event.is_action_pressed("ui_down") and key:
+		key = false
+		timer.start()
 		$re.play()
-	elif event.is_action_pressed("ui_left"):
+	elif event.is_action_pressed("ui_left") and key:
+		key = false
+		timer.start()
 		$mi.play()
-	elif event.is_action_pressed("ui_right"):
+	elif event.is_action_pressed("ui_right") and key:
+		key = false
+		timer.start()
 		$fa.play()
 
 func _on_Area2D_body_entered(body):
@@ -61,3 +75,7 @@ func _on_Area2D_body_entered(body):
 func _on_Area2D_body_exited(body):
 	if body is StaticBody2D:
 		state = JUMP
+
+
+func _on_Timer_timeout():
+	key = true
